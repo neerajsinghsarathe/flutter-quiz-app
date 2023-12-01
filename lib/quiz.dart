733ca4aa98b8d13769c058d8ebfe9ce0cafeeeaf
Neeraj Singh Sarathe <66715864+neerajsinghsarathe@users.noreply.github.com>
2023-12-01 22:30:59 +0530

    Alternative for initState()

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -11,21 +11,27 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
+  var activeScreen = 'start-screen';
 
-  @override
+  /*@override
   void initState() {
     activeScreen = StartScreen(switchScreen);
     super.initState();
-  }
+  }*/
 
   void switchScreen(){
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'question-screen';
     });
   }
   @override
   Widget build(BuildContext context) {
+    //Alternative 2 instead of init State Calling
+    Widget screenWidget = StartScreen(switchScreen);
+    if(activeScreen == 'question-screen'){
+      screenWidget = const QuestionsScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -39,7 +45,11 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen,
+          // Alternative 1 instead of init State Calling
+          /*child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const Quiz(),*/
+          child: screenWidget,
         ),
       ),
     );
