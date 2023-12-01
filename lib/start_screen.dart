d2b1a20a850d2cc2da7aab8b8e10bcaecb769b27
Neeraj Singh Sarathe <66715864+neerajsinghsarathe@users.noreply.github.com>
2023-12-01 22:06:50 +0530

    Lifecycle and Rendering explained

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -12,7 +14,7 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             "assets/images/quiz-logo.png",
             width: 300,
-            color: const Color.fromARGB(150, 255, 255, 255),
+            color: const Color.fromARGB(150, 255, 255, 255)
           ),
           const SizedBox(
             height: 80,
@@ -25,7 +27,7 @@ class StartScreen extends StatelessWidget {
             height: 30,
           ),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: startQuiz,
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
