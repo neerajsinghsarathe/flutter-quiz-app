4e89a793837ae8f162d0d8d416be2a427b2b6911
Neeraj Singh Sarathe <66715864+neerajsinghsarathe@users.noreply.github.com>
2023-12-04 01:09:47 +0530

    created reusable button and integrated with questions screen.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
-class QuestionsScreen extends StatefulWidget{
+class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
+
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -12,6 +14,19 @@ class QuestionsScreen extends StatefulWidget{
 class _QuestionsState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('questions screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The questions...', style: TextStyle(color: Colors.white),),
+          const SizedBox(height: 30),
+          AnswerButton(answerText: 'Answer 1', onTap: () { },),
+          AnswerButton(answerText: 'Answer 2', onTap: () { },),
+          AnswerButton(answerText: 'Answer 3', onTap: () { },),
+          AnswerButton(answerText: 'Answer 4', onTap: () { },),
+        ],
+      ),
+    );
   }
 }
