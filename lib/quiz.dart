dd2b845117202a225d2f4c6a041fd5f05d3d21e9
Neeraj Singh Sarathe <66715864+neerajsinghsarathe@users.noreply.github.com>
2023-12-13 06:00:16 +0530

    Created Results Screen and get all the answers in it.

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/results_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
 import 'data/questions.dart';
@@ -21,8 +22,7 @@ class _QuizState extends State<Quiz> {
 
     if (selectedAnswers.length == questions.length){
       setState(() {
-        selectedAnswers = [];
-        activeScreen = 'start-screen';
+        activeScreen = 'results-screen';
       });
     }
   }
@@ -38,6 +38,10 @@ class _QuizState extends State<Quiz> {
     Widget screenWidget = StartScreen(switchScreen);
     if(activeScreen == 'question-screen'){
       screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer,);
+    }
+    
+    if (activeScreen == 'results-screen'){
+      screenWidget = ResultsScreen(chosenAnswers: selectedAnswers,);
     }
 
     return MaterialApp(
